8d6289b6a1f903d799c73aee88d82b4c1c424d71
Sushant9412 <mishrasushant143.sm@gmail.com>
2025-07-24 12:28:16 +0530

    dashboard menu Complete

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -8,14 +8,14 @@ import Holdings from "./Holdings";
 import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
-// import WatchList from "./WatchList";
+import WatchList from "./WatchList";
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
-      {/* <GeneralContextProvider>
+     
         <WatchList />
-      </GeneralContextProvider> */}
+     
       <div className="content">
         <Routes>
           <Route exact path="/" element={<Summary />} />
